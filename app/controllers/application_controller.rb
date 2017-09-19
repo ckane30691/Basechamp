91232f41c355d46e7d0f1f1cf4bea633da28b30f
App Academy Student <student@appacademy.io>
2017-09-19 14:07:43 -0700

    create user & session controller and update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,31 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :logged_in?, :current_user
+
+  def login!(user)
+    session[:session_token] = user.reset_session_token!
+    @current_user = user
+  end
+
+  def logout!
+    session[:session_token] = nil
+    current_user.reset_session_token!
+    @current_user = nil
+  end
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_logged_in
+    unless current_user
+      render json: { base: ["Invalid Credentials"] }, status: 401
+    end
+  end
+
 end
