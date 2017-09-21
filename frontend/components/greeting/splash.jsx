c0f1d77b889f421cadf2b72306ad2dda5ccf901f
Cory Kane <ckane30691@gmail.com>
2017-09-20 23:38:15 -0700

    Add more styling to splash page

--- frontend/components/greeting/splash.jsx
+++ frontend/components/greeting/splash.jsx
@@ -2,21 +2,37 @@ import React from 'react';
 import SessionFormContainer from '../session_form/session_form_container';
 
 const Splash = () => (
-	<div className='splash-body-container'>
+	<div className='splash'>
 		<div className="hero-img-container">
 			<img className="hero-img" alt="hero image" src="https://s3-us-west-1.amazonaws.com/basechamp/basecamp_hero.jpg"/>
 		</div>
 
-		<div className="intro-container">
-			<h1 className="intro-title">We've been expecting you.</h1>
+			<div className="intro-container">
+				<h1 className="intro-title">We've been expecting you.</h1>
 
-			<p className="intro-body">
-				All growing businesses run into the same fundamental problems.
-				Hair on fire, buried under email, stuff everywhere.
-				The good news? Basechamp solves them.
-			</p>
-		</div>
-		<SessionFormContainer />
+				<p className="intro-body">
+					All growing businesses run into the same fundamental problems.
+					Hair on fire, buried under email, stuff everywhere.
+					The good news? Basechamp solves them.
+				</p>
+			</div>
+
+
+			<div className="splash-signup">
+				<div className="call-to-action">
+					<h3>Try Basechamp Today:</h3>
+
+					<ul>
+						<li>Everything will be in one organized place</li>
+						<li>Everyone will be on the same page</li>
+						<li>Projects will get off the ground faster</li>
+						<li>You'll know exactly what’s going on</li>
+						<li>You'll free up time spent on meetings</li>
+					</ul>
+				</div>
+
+				<SessionFormContainer />
+			</div>
 	</div>
 )
 
--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -10,7 +10,7 @@ const Auth = ({component: Component, path, loggedIn }) => (
 
 const Protected = ({component: Component, path, loggedIn }) => (
   <Route path={path} render={props => (
-      loggedIn ? <Component {...props} /> : <Redirect to="/login" />
+      loggedIn ? <Component {...props} /> : <Redirect to="/" />
   )} />
 );
 
