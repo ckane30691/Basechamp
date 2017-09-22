342010a802da82aa775b1b027e2d688eb82d06fa
Cory Kane <ckane30691@gmail.com>
2017-09-22 11:24:00 -0700

    Finish styling for project show page

--- frontend/components/projects/project_show.jsx
+++ frontend/components/projects/project_show.jsx
@@ -10,13 +10,28 @@ class ProjectShow extends React.Component {
 	render() {
 
 		return (
-			<div className="todo-link-container">
-				<Link to={`/projects/${this.props.project.id}/todos`}>
-					To-dos
-				</Link>
-				<hr/>
-				<img src="https://s3-us-west-1.amazonaws.com/basechamp/todos_icon.png" />
-				<p>Make lists of work that needs to get done.</p>
+			<div className="project-show-body">
+				<div className="generic-show-container">
+					<Link className="generic-show-title" to={`/projects/${this.props.match.params.projectId}/todos`}>
+						To-dos
+					</Link>
+					<hr/>
+					<img className="generic-show-img" src="https://s3-us-west-1.amazonaws.com/basechamp/checkmark.png" />
+					<p className="generic-show-desc">Make lists of work that needs to get done.</p>
+				</div>
+
+				<div className="generic-show-container">
+					<h1 className="generic-show-title">Comming Soon</h1>
+					<hr />
+					<img className="generic-show-img" src="https://s3-us-west-1.amazonaws.com/basechamp/todos_icon.png" />
+				</div>
+
+				<div className="generic-show-container">
+					<h1 className="generic-show-title">Comming Soon</h1>
+					<hr />
+					<img className="generic-show-img" src="https://s3-us-west-1.amazonaws.com/basechamp/calendar_icon.png" />
+				</div>
+
 			</div>
 		)
 	}
