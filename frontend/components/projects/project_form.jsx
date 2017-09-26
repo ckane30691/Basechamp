a5c5b113aecafe1dba4ed7ca779def1bb2ea40eb
Cory Kane <ckane30691@gmail.com>
2017-09-25 22:13:53 -0700

    Fix refresh bug in project edit/new form

--- frontend/components/projects/project_form.jsx
+++ frontend/components/projects/project_form.jsx
@@ -29,12 +29,13 @@ class ProjectForm extends React.Component {
 	}
 
 	render() {
-
+		const title = this.state ? this.state.title : "";
+		const desc = this.state ? this.state.description : "";
 		return(
 			<div className="new-project-container">
 				<form onSubmit={this.handleSubmit}>
 					<input required type="text"
-						value={this.state.title}
+						value={title}
 						onChange={this.update('title')}
 						className="project-input pti"
 						placeholder="Name this project" />
@@ -42,7 +43,7 @@ class ProjectForm extends React.Component {
 					<br/>
 
 					<input type="text"
-						value={this.state.description}
+						value={desc}
 						onChange={this.update('description')}
 						className="project-input pdi"
 						placeholder="Project Description" />
