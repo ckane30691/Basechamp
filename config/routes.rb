d6040c1428a6aa17a446b358b4e6c6b1683604e3
Cory Kane <ckane30691@gmail.com>
2017-09-26 09:34:41 -0700

    write api routes for events feature

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,14 @@ Rails.application.routes.draw do
     resources :projects, except: %i(edit new) do
       resources :todos, only: %i(index create)
       resources :messages, only: %i(index create)
+      resources :events, only: %i(index create)
     end
     resources :todos, only: %i(show update destroy)
     resources :messages, only: %i(show update destroy) do
       resources :comments, only: %i(index create)
     end
     resources :comments, only: [:destroy]
+    resources :events, except: %i(edit new index create)
   end
 
   root 'static_pages#root'
