0f37b2fb73b794b00427bcbc1d9d2c58292a00ba
Cory Kane <ckane30691@gmail.com>
2024-10-09 21:53:49 -0700

    Move webpack output to javascripts

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
   context: __dirname,
   entry: "./frontend/basechamp_entry.jsx",
   output: {
-    path: path.resolve(__dirname, 'public', 'assets'),
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
   },
   plugins: plugins,
